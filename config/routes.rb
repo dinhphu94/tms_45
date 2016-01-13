d4308945e0ac9b015e5959bb9b26905becf78d0c
Ngoc Vu <ngocvu.uet@gmail.com>
2016-01-13 22:59:47 +0700

    task/51130

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get "help" => "static_pages#help"
   get "about" => "static_pages#about"
   get "contact" => "static_pages#contact"
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
 end
