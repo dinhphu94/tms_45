54cdc1cfb98507a90932bfab5d887a989e3f8f93
Ngoc Vu <ngocvu.uet@gmail.com>
2016-01-14 10:18:38 +0700

    task/51136

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :load_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t :profile_updated
+      redirect_to root_url
+    else
+      render :edit
+    end
+  end
+
+  private
+  def load_user
+    @user = User.find_by id: params[:id]
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+      :password_confirmation)
+  end
+
+  def correct_user
+    redirect_to(root_url) unless current_user? @user
+  end
+end
