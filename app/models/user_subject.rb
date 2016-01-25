c35418014cffc3a7fb82c51f98c5e932a04a1070
NeverSmilek57CLC <neversmile12b1@gmail.com>
2016-01-25 13:01:40 +0700

    refs #51135 show user profile by role

--- app/models/user_subject.rb
+++ app/models/user_subject.rb
@@ -2,7 +2,12 @@ class UserSubject < ActiveRecord::Base
   belongs_to :user
   belongs_to :subject
   belongs_to :user_course
+
   has_many :user_tasks
 
   accepts_nested_attributes_for :user_tasks, reject_if: proc{|a| a[:task_id] == "0"}
+
+  scope :has_user_with_course, ->(user, user_course) {
+    where "user_id = ? AND user_course_id = ?", user.id, user_course.id
+  }
 end
